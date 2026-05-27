23dc9e6f42256bf4df27557eb73f1293baf0ff4b
Paweł Mstowski <pawel.mstowski@gmail.com>
2026-05-27 20:33:36 +0200

    Get key from key mapping instead of env variable

--- cmd/keys/gen/gen.go
+++ cmd/keys/gen/gen.go
@@ -3,6 +3,7 @@ package gen
 import (
 	"log"
 
+	"github.com/mustafmst/universal-repo-vault/internal/repo"
 	"github.com/mustafmst/universal-repo-vault/internal/vault"
 	"github.com/spf13/cobra"
 )
@@ -11,11 +12,16 @@ var GenKeyCmd = &cobra.Command{
 	Use:   "gen",
 	Short: "Generates new encryption key and assigns it to  a repo in user wide configuration",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		repoPath, err := repo.GetCurrentRepoPath()
+		if err != nil {
+			log.Println("Current path not in git repository")
+			return nil
+		}
 		key, err := vault.GenNewKey()
 		if err != nil {
 			return err
 		}
-		err = vault.SaveKey(key, "tmp")
+		err = vault.SaveKeyWithRepoName(key, repoPath)
 		if err != nil {
 			return err
 		}
--- internal/vault/key.go
+++ internal/vault/key.go
@@ -8,11 +8,64 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/mustafmst/universal-repo-vault/internal/files"
+	"go.yaml.in/yaml/v3"
 )
 
 const keyVariable string = "URV_KEY_NAME"
 
 const keyLength int = 32
+
+type KeyMapping struct {
+	keys map[string]string
+}
+
+func (k *KeyMapping) Get(repo string) (string, error) {
+	key, ok := k.keys[repo]
+	if !ok {
+		return "", fmt.Errorf("key for repo not found: %s", repo)
+	}
+	return key, nil
+}
+
+func (k *KeyMapping) Add(repo, keyName string, replace bool) error {
+	_, ok := k.keys[repo]
+	if ok && !replace {
+		return fmt.Errorf("key exists for this repo, use --force to change")
+	}
+	k.keys[repo] = keyName
+	return nil
+}
+
+func (k *KeyMapping) Save() error {
+	data, err := yaml.Marshal(&k.keys)
+	if err != nil {
+		return fmt.Errorf("serializing key mapping file: %w", err)
+	}
+	_, err = files.SaveDataToFile(filepath.Join(os.Getenv("HOME"), ".config", "urv", "mapping.yaml"), data, true)
+	if err != nil {
+		return fmt.Errorf("saving key mapping file: %w", err)
+	}
+	return nil
+}
+
+func NewKeyMapping() (*KeyMapping, error) {
+	file := filepath.Join(os.Getenv("HOME"), ".config", "urv", "mapping.yaml")
+	rawData, err := os.ReadFile(file)
+	if err != nil && errors.Is(err, os.ErrNotExist) {
+		return &KeyMapping{make(map[string]string)}, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("reading key mapping file: %w", err)
+	}
+	keys := make(map[string]string)
+	err = yaml.Unmarshal(rawData, &keys)
+	if err != nil {
+		return nil, fmt.Errorf("deserializing key mapping file: %w", err)
+	}
+	return &KeyMapping{keys}, nil
+}
 
 func GenNewKey() (string, error) {
 	keyBytes := make([]byte, keyLength)
@@ -22,15 +75,13 @@ func GenNewKey() (string, error) {
 	return hex.EncodeToString(keyBytes), nil
 }
 
-func SaveKey(key string, repoPath string) error {
-	// TODO: Partial implementation, in future use user config and repo name to get right key
+func SaveKeyWithRepoName(key, repoPath string) error {
+	return SaveKey(key, repoPath, filepath.Base(repoPath))
+}
+
+func SaveKey(key string, repoPath string, keyName string) error {
 	if err := ensureKeysDir(); err != nil {
 		return fmt.Errorf("creating keys directory: %w", err)
-	}
-
-	keyName := os.Getenv(keyVariable)
-	if keyName == "" {
-		return fmt.Errorf("key name not set")
 	}
 
 	keyFile := filepath.Join(os.Getenv("HOME"), ".config", "urv", "keys", keyName)
@@ -53,15 +104,27 @@ func SaveKey(key string, repoPath string) error {
 	}
 	log.Printf("DEBUG: key lenght written: %d, set key lenght: %d", n, keyLength)
 
-	return nil
+	mapping, err := NewKeyMapping()
+	if err != nil {
+		return err
+	}
+	err = mapping.Add(repoPath, keyName, true)
+	if err != nil {
+		return err
+	}
+	return mapping.Save()
 }
 
 func GetKeyForRepo(repoPath string) (string, error) {
 	result := ""
 
-	keyName := os.Getenv(keyVariable)
-	if keyName == "" {
-		return result, fmt.Errorf("key name not set")
+	mapping, err := NewKeyMapping()
+	if err != nil {
+		return "", err
+	}
+	keyName, err := mapping.Get(repoPath)
+	if err != nil {
+		return "", err
 	}
 
 	keyFile := filepath.Join(os.Getenv("HOME"), ".config", "urv", "keys", keyName)
